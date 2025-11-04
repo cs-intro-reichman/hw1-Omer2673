8a77aed39d8137ad13a4e7ac32b398008e89e4ab
Omer Hoz <omer.hoz@post.runi.ac.il>
2025-11-04 19:44:41 +0200

    NumWords

--- NumWords.java
+++ NumWords.java
@@ -2,9 +2,9 @@
 public class NumWords {
 	public static void main(String args[]) {
 		int num = Integer.parseInt(args[0]);
-		int ones = num % 10;
+		 int ones = num % 10;
         int tens = (num / 10) % 10;
-        int hundreds = (num / 100) % 10;
+        int hundreds = num / 100;  
 
         System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 	    
