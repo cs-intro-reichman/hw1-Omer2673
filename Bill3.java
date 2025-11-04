8dc81729c066f0e7c4dee9bd937a2a658732312b
Omer Hoz <omer.hoz@post.runi.ac.il>
2025-11-04 18:36:15 +0200

    Bill3

--- Bill3.java
+++ Bill3.java
@@ -1,7 +1,5 @@
-// Splits a restaurant bill evenly among three diners.
 public class Bill3 {
     public static void main(String[] args) {
-        // קבלת שמות ושליחת חשבון שווה לכולם
         String name1 = args[0];
         String name2 = args[1];
         String name3 = args[2];
@@ -9,11 +7,6 @@ public class Bill3 {
 
         double amount = Math.ceil(bill / 3.0);
 
-        System.out.println("Dear " + name1 + ", " + name2 + " and " + name3 + ": pay " + amount + " shekels each");
+        System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + amount + " Shekels each");
     }
 }
-
-
-
-
-
